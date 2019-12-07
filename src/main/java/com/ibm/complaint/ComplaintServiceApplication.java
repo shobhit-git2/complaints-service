9438ed2bc15c6f7d45f8604c59c7ecf08cbdbd47
shobhit-git2 <shobhit51924@gmail.com>
2019-12-07 20:54:16 +0530

    Update ComplaintServiceApplication.java

--- src/main/java/com/ibm/complaint/ComplaintServiceApplication.java
+++ src/main/java/com/ibm/complaint/ComplaintServiceApplication.java
@@ -2,7 +2,6 @@ package com.ibm.complaint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
