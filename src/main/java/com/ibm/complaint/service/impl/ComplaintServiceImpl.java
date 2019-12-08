fdf7eb0cd606ff9e9cb3a7028da4eb9ff4074250
shobhit-git2 <shobhit51924@gmail.com>
2019-12-08 20:13:23 +0530

    Update ComplaintServiceImpl.java

--- src/main/java/com/ibm/complaint/service/impl/ComplaintServiceImpl.java
+++ src/main/java/com/ibm/complaint/service/impl/ComplaintServiceImpl.java
@@ -10,14 +10,17 @@ import org.springframework.stereotype.Service;
 import com.ibm.complaint.ComplaintConstants;
 import com.ibm.complaint.exception.NoComplaintsFoundException;
 import com.ibm.complaint.model.Complaint;
-import com.ibm.complaint.repository.ComplaintRepository;
+import com.ibm.complaint.repository.ComplaintMongoRepository;
 import com.ibm.complaint.service.ComplaintService;
 
 @Service
 public class ComplaintServiceImpl implements ComplaintService {
 
+	//@Autowired
+	//private ComplaintRepository repo;
+	
 	@Autowired
-	private ComplaintRepository repo;
+	private ComplaintMongoRepository complaintrepo;
 
 	@Override
 	public String registerComplaint(Complaint complaint) {
@@ -26,26 +29,28 @@ public class ComplaintServiceImpl implements ComplaintService {
 		complaint.setCreatedDate(new Date());
 		complaint.setAssignedTo(ComplaintConstants.DEFAULT_MAINTENANCE_TEAM);
 		complaint.setOperationType(ComplaintConstants.DEFAULT_OPERATION_TYPE);
-		String result = repo.add(complaint);
-
-		return result;
+		//String result = repo.add(complaint);
+		Complaint obj = (Complaint) complaintrepo.save(complaint);
+		return obj.getId();
 	}
 
 	@Override
 	public List<Complaint> getComplaintsByUser(String username, String status, String groupType)
 			throws NoComplaintsFoundException {
-		List<Complaint> comList = repo.getComplaintsByUser(username, status, groupType);
-		return comList;
+		//List<Complaint> comList = repo.getComplaintsByUser(username, status, groupType);
+		//return comList;
+		return null;
 	}
 
 	@Override
 	public Complaint getComplaintDetails(String id) throws NoComplaintsFoundException {
 		Complaint comList;
-		try {
-			comList = repo.getComplaintDetails(id);
-		} catch (IOException e) {
-			throw new NoComplaintsFoundException("Error occurred due to:" + e.getMessage());
-		}
+		/*
+		 * try { comList = repo.getComplaintDetails(id); } catch (IOException e) { throw
+		 * new NoComplaintsFoundException("Error occurred due to:" + e.getMessage()); }
+		 * return comList;
+		 */
+		comList = complaintrepo.findBy_id(id);
 		return comList;
 	}
 
